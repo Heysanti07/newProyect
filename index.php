7b44ed1613ea49837890c71bf4fb36e56f4deb79
J-Fernando-PM <psptodopsppower2015@gmail.com>
2025-03-05 17:46:19 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -14,19 +14,17 @@
       <div class="row justify-content-center">
         <div class="col-md-4">
           <h2 class="text-center">Iniciar sesión</h2>
-          <?php if ($mensaje): ?>
-          <div class="alert alert-danger"><?= $mensaje ?></div>
-          <?php endif; ?>
+         
           <form method="POST">
             <div class="mb-3">
-              <label class="form-label">Usuario</label>
-              <input type="text" name="usuario" class="form-control" required />
+              <label class="form-label">Correo</label>
+              <input type="email" name="correo" class="form-control" required />
             </div>
             <div class="mb-3">
               <label class="form-label">Contraseña</label>
               <input
                 type="password"
-                name="password"
+                name="contrasena"
                 class="form-control"
                 required
               />
@@ -34,7 +32,7 @@
             <button type="submit" class="btn btn-primary w-100">Entrar</button>
           </form>
           <p class="text-center mt-3">
-            ¿No tienes cuenta? <a href="register.html">Regístrate aquí</a>
+            ¿No tienes cuenta? <a href="register.php">Regístrate aquí</a>
           </p>
         </div>
       </div>
